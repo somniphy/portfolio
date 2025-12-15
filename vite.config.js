eefe3c3e12032b308c701ecb68eb7c1a5ed10a43
somniphy <penalescjay@gmail.com>
2025-12-15 14:01:50 +0800

    works page error fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
       input: {
         main: "index.html",
         about: "about.html",
+        works: "works.html",
         contact: "contact.html",
       },
     },
