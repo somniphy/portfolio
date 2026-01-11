8f9daf144d992b89e6e363344543e8d9e4f003f9
somniphy <penalescjay@gmail.com>
2026-01-11 12:36:07 +0800

    about page fix 4

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: "index",
-        about: "about",
+        main: "index.html",
+        about: "about.html",
       },
     },
   },
